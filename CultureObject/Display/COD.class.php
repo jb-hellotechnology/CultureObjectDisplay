6815b5ceeb309908a4254c2a862829e2a3eaa48f
Jack Barber <80117647+jb-hellotechnology@users.noreply.github.com>
2025-04-16 12:28:14 +0100

    MDS changes to support 'all fields' output
    New function to match all fields display option. Also changes to other callback to dynamically create headings, so that headings for empty fields are not displayed.

--- CultureObject/Display/COD.class.php
+++ CultureObject/Display/COD.class.php
@@ -47,7 +47,21 @@ class COD  {
         $html = preg_replace_callback(
             '/{{cos.field_value.([\w\-_\ ]+)}}/',
             function ($matches) {
-                return cos_get_field($matches[1]);
+                $field = cos_get_field($matches[1]);
+                if($field){
+                    $heading = ucfirst(str_replace("_", " ", $matches[1]));
+                    return '<h3><strong>'.$heading.'</strong></h3><p>'.$field.'</p>';
+                }
+            },
+            $html
+        );
+        
+        // FUNCTION TO MATCH 'ALL_FIELDS' OPTION
+        $html = preg_replace_callback(
+            '/{{cos.all_fields}}/',
+            function () {
+                // FUNCTION TO RETURN ALL FIELDS AS HTML TABLE
+                return cos_fields();
             },
             $html
         );
@@ -111,8 +125,8 @@ class COD  {
         wp_register_script('cod_admin_js', $this->plugin_url . '/js/culture-object-display.js?nc='.time(), array('jquery'), '1.0.0', true);
         wp_enqueue_script('cod_admin_js');*/
         
-		add_action('media_buttons', array($this, 'add_cod_button'), 20);
-		add_action('admin_print_footer_scripts', array($this, 'add_mce_popup'));
+        add_action('media_buttons', array($this, 'add_cod_button'), 20);
+        add_action('admin_print_footer_scripts', array($this, 'add_mce_popup'));
     }
     
     function add_mce_popup() {
@@ -131,40 +145,40 @@ class COD  {
             $provider_class = new $provider['class'];
             $fields = $provider_class->register_remappable_fields();
 
-		?>
-		<script>
-	        jQuery(document).on('click', '.add_field_name', function() {
-    	        val = '{{cos.field_name.'+jQuery('#add_field').val()+'}}';
-				window.send_to_editor(val);
-				self.parent.tb_remove();
+        ?>
+        <script>
+            jQuery(document).on('click', '.add_field_name', function() {
+                val = '{{cos.field_name.'+jQuery('#add_field').val()+'}}';
+                window.send_to_editor(val);
+                self.parent.tb_remove();
             });
-	        jQuery(document).on('click', '.add_field_value', function() {
-    	        val = '{{cos.field_value.'+jQuery('#add_field').val()+'}}';
-				window.send_to_editor(val);
-				self.parent.tb_remove();
+            jQuery(document).on('click', '.add_field_value', function() {
+                val = '{{cos.field_value.'+jQuery('#add_field').val()+'}}';
+                window.send_to_editor(val);
+                self.parent.tb_remove();
             });
-		</script>
-		
-		<div id="cod_media_link" style="display:none;">
+        </script>
+        
+        <div id="cod_media_link" style="display:none;">
             <h2><?php esc_attr_e('Add Field', 'culture-object-display'); ?></h2>
 
             <select name="add_field" id="add_field">
                 <?php foreach($fields as $key => $field) { ?>
-            	<option value="<?php echo $key; ?>"><?php echo $field; ?></option>
-            	<?php } ?>
+                <option value="<?php echo $key; ?>"><?php echo $field; ?></option>
+                <?php } ?>
             </select><br />
             
             <a style="margin: 10px 10px 0 0;" href="#" class="button add_field_name" title="<?php esc_attr_e( 'Add Field Name', 'culture-object-display'); ?>"><div><?php esc_html_e('Add Field Name', 'culture-object-display'); ?></div></a>
             <a style="margin-top: 10px" href="#" class="button add_field_value" title="<?php esc_attr_e( 'Add Field Value', 'culture-object-display'); ?>"><div><?php esc_html_e('Add Field Value', 'culture-object-display'); ?></div></a>
         </div>
 
-	<?php
+    <?php
         }
-	}
+    }
     
     function add_cod_button() {
-		echo '<a href="#TB_inline?width=600&height=300&inlineId=cod_media_link" class="button thickbox cod_media_link" id="add_cod" title="'.esc_attr__( 'Add Culture Object Field', 'culture-object-display' ).'"><div>'.esc_html__( 'Add Culture Object Field', 'culture-object-display' ).'</div></a>';
-	}
+        echo '<a href="#TB_inline?width=600&height=300&inlineId=cod_media_link" class="button thickbox cod_media_link" id="add_cod" title="'.esc_attr__( 'Add Culture Object Field', 'culture-object-display' ).'"><div>'.esc_html__( 'Add Culture Object Field', 'culture-object-display' ).'</div></a>';
+    }
     
     function add_menu_page() {
         $display_page = add_submenu_page('cos_settings', __('Display Settings', 'culture-object-display'), __('Display Settings', 'culture-object-display'), 'administrator', 'cos_display_settings', array($this,'generate_display_page'));
